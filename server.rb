137b2d06a30a4bb1c312ed59a2caf441558558a3
askarf <ayalakarf@gmail.com>
2023-12-12 13:25:06 -0500

    creates a filter for the url

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sinatra/namespace"
 require "mongoid"
 
 Mongoid.load! "mongoid.config"
@@ -16,8 +17,45 @@ class Book
 
   index({ title: "text" })
   index({ isbn: 1 }, { unique: true, name: "isbn_index" })
+
+  scope :title, ->(title) { where(title: /^#{title}/) }
+  scope :isbn, ->(isbn) { where(isbn: isbn) }
+  scope :author, ->(author) { where(author: author) }
+end
+
+class BookSerializer
+  def initialize(book)
+    @book = book
+  end
+
+  def as_json(*)
+    data = {
+      id: @book.id.to_s,
+      title: @book.title,
+      author: @book.author,
+      isbn: @book.isbn,
+    }
+    data[:errors] = @book.errors if @book.errors.any?
+    data
+  end
 end
 
 get "/" do
   "Welcome to Booklist!"
+end
+
+namespace "/api/v1" do
+  before do
+    content_type "application/json"
+  end
+
+  get "/books" do
+    books = Book.all
+
+    [:title, :isbn, :author].each do |filter|
+      books = books.send(filter, params[filter]) if params[filter]
+    end
+
+    books.map { |book| BookSerializer.new(book) }.to_json
+  end
 end
